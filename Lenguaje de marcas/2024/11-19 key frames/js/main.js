d0f7a570850d2610e1e9d2902f3dd8b3f3400d95
OleksandrTat <ifpoleksandrtataryn>
2024-12-20 10:20:31 +0100

    lenguaje de marcas

---
+++ Lenguaje de marcas/2024/11-19 key frames/js/main.js
@@ -0,0 +1,19 @@
+// let nombre = "precio de  los palotes"
+
+// //CONSOLE LOG
+// console.log(`Hola, mi nombre es ${nombre}`)
+
+
+// //DOCUMENT WRITE
+// document.write(`<h1>${nombre}</h1>`)
+// document.write(`<a href="file:///D:/Lenguaje%20de%20marcas/11-19%20key%20frames/index.html">${nombre}</a>`)
+// document.write(`<h1>` + new Date() + `</h1>`)
+// document.write("<h2>ScreenWidth: " + screen.width + " px</h2>")
+// document.write("<h1>Ultima modificion" + window.document.lastModified + "</h1>")
+// document.write("<h1>Datos sobre ti" + navigator.userAgent + "</h1>")
+
+
+function cambiarNombre(){
+    const contenido = document.querySelector(".cambiar");
+    contenido.textContent = "1000 dias de la guerra"
+}
